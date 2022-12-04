b59f2583487675983f22e9dbcc5eb92bdcb2a7cd
mewore <46766863+mewore@users.noreply.github.com>
2022-12-04 03:31:07 +0200

    Add a test-everything.sh script

---
+++ test-everything.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./gradlew --parallel frontend:frontendCheckDisabledLintRules backend:bootJar jar frontend:frontendTest \
+  backend:spotbugsMain backend:test imagediary:spotbugsMain imagediary:test
+exit $?
